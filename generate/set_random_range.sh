839c1319b98a870c17c33edebb20269329699dc9
catchchaos <param.catchchaos@gmail.com>
2016-10-13 23:48:34 +0530

    scripts updated, TODO readme

--- generate/set_random_range.sh
+++ generate/set_random_range.sh
@@ -2,9 +2,9 @@
 
 random_range=$1
 
-sed -i "14s/.*/    x1 = random.randint(-$random_range, $random_range)/" create_points.py 
-sed -i "15s/.*/    y1 = random.randint(-$random_range, $random_range)/" create_points.py 
-sed -i "16s/.*/    z1 = random.randint(-$random_range, $random_range)/" create_points.py 
+sed -i "14s/.*/    x1 = random.randint(-$random_range, $random_range)/" raw.py 
+sed -i "15s/.*/    y1 = random.randint(-$random_range, $random_range)/" raw.py 
+sed -i "16s/.*/    z1 = random.randint(-$random_range, $random_range)/" raw.py 
 
 exit
 
